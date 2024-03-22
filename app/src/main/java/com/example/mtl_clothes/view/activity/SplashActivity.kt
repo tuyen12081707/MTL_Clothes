8d4c5cda74462476b3ad54e6b80b5759739ec404
tuyen12081707 <105075028+tuyen12081707@users.noreply.github.com>
2024-03-22 20:04:22 +0700

    update splash

--- app/src/main/java/com/example/mtl_clothes/view/activity/SplashActivity.kt
+++ app/src/main/java/com/example/mtl_clothes/view/activity/SplashActivity.kt
@@ -1,6 +1,8 @@
 package com.example.mtl_clothes.view.activity
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
@@ -14,7 +16,9 @@ import com.example.mtl_clothes.viewmodel.CommonVM
 
 class SplashActivity : BaseActivity<ActivitySplashBinding, CommonVM>() {
     override fun initView() {
+        Handler(Looper.getMainLooper()).postDelayed({
 
+        },3000)
     }
 
     override fun getClassVM(): Class<CommonVM> {
